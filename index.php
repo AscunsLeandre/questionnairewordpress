fb4d0c726f6235d4e1e45e303401399e4785eb00
AscunsLeandre <ascuns.contact@gmail.com>
2017-11-13 10:23:48 +0100

    Changement index tu fait du caca arthur

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php get_header(); ?>
-<<<<<<< HEAD
     <h1>Hello World ca va OUII BIEN ca va </h1>
     <?php while (have_posts()):the_post();?>
       <article>
         <header>
           <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-=======
-    <h1>Hello World ca va OUII BIEN ca va  ? </h1>
-    <?php while (have_posts()):the_post;?>
-      <article>
-        <header>
-          <h2><?php the_title(); ?></h2>
->>>>>>> 1f1f25a38e8881a96e72d4d652af504e5ac4e390
         </header>
         <div>
           <?php the_content(); ?>
