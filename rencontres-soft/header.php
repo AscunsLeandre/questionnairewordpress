3439836ec82ec88f67f9c63b098d5e47978d69f7
AscunsLeandre <ascuns.contact@gmail.com>
2017-11-13 08:42:56 +0100

    Changements

--- rencontres-soft/header.php
+++ rencontres-soft/header.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html>
 	<head>
- 		<?php wp_head() ?>
+ 		<?php wp_head();?>
 	</head>
 	<body>
 	<header>
 		<h1><?php bloginfo('name'); ?></h1>
-		<h1><?php bloginfo('name'); ?></h1>
-
+		<h1><?php bloginfo('description'); ?></h1>
 	</header>
