33e3d0535ac1502eb6557cbdeffa1b5aa2960e84
AscunsLeandre <ascuns.contact@gmail.com>
2017-11-13 09:14:31 +0100

    Bim bam boum

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
     <h1>Hello World ca va OUII BIEN ca va  ? </h1>
     <?php while (have_posts()):the_post;?>
+      <article>
+        <header>
+          <h2><?php the_title(); ?></h2>
+        </header>
+        <div>
+          <?php the_content(); ?>
+        </div>
+      </article>
     <?php endwhile; ?>
 <?php get_footer(); ?>
