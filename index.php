365554e1d5aa9fbe80b860f367c2bd3f6e5745d9
AscunsLeandre <ascuns.contact@gmail.com>
2017-11-13 09:35:50 +0100

    Ca fonctionne

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
-    <h1>Hello World ca va OUII BIEN ca va  ? </h1>
+    <h1>Hello World ca va OUII BIEN ca va </h1>
     <?php while (have_posts()):the_post;?>
       <article>
         <header>
-          <h2><?php the_title(); ?></h2>
+          <h2><a href="<?php the_permalink;?>"></a><?php the_title(); ?></h2>
         </header>
         <div>
           <?php the_content(); ?>
