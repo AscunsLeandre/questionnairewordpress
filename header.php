5eb8b50583803f5786bc4a5582f8cda97f99c122
AscunsLeandre <ascuns.contact@gmail.com>
2017-11-24 08:44:24 +0100

    TOUT VA CHANGER

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 	<body>
 	<header>
 		<h1><?php bloginfo('name'); ?></h1>
-		<h2><?php bloginfo('description'); ?></h1>
+		<h2 class="sous_titre"><?php bloginfo('description'); ?></h1>
 		<nav>
 			<?php wp_page_menu('show_home=1') ?>
 		</nav>
